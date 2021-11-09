a4c011eae8cafa7b69bf5110ee034179e30cc2ab
FieryInferno <bagassetia271@gmail.com>
2021-11-09 10:09:38 +0700

    Delete total siswa in dashboard

--- application/views/tata_usaha/dashboard.php
+++ application/views/tata_usaha/dashboard.php
@@ -13,25 +13,7 @@
       </div>
       <!-- Card stats -->
       <div class="row">
-        <div class="col-xl-3 col-md-6">
-          <div class="card card-stats">
-            <!-- Card body -->
-            <div class="card-body">
-              <div class="row">
-                <div class="col">
-                  <h5 class="card-title text-uppercase text-muted mb-0">Total Siswa</h5>
-                  <span class="h2 font-weight-bold mb-0"><?= count($totalSiswa); ?></span>
-                </div>
-                <div class="col-auto">
-                  <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
-                    <i class="ni ni-active-40"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
+        <div class="col-xl-4 col-md-6">
           <div class="card card-stats">
             <!-- Card body -->
             <div class="card-body">
@@ -49,7 +31,7 @@
             </div>
           </div>
         </div>
-        <div class="col-xl-3 col-md-6">
+        <div class="col-xl-4 col-md-6">
           <div class="card card-stats">
             <!-- Card body -->
             <div class="card-body">
@@ -67,7 +49,7 @@
             </div>
           </div>
         </div>
-        <div class="col-xl-3 col-md-6">
+        <div class="col-xl-4 col-md-6">
           <div class="card card-stats">
             <!-- Card body -->
             <div class="card-body">
